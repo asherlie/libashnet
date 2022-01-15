ea1bd51bdcc7f18a84e3f0d4b844f91ae5cbb637
asherlie <lieber.asher@gmail.com>
2022-01-15 00:33:17 -0500

    updated client to correctly print ppped messages

--- cli.py
+++ cli.py
@@ -5,7 +5,7 @@ mq_out = sysv_ipc.MessageQueue(857123040)
 
 def recv():
     if mq_out.current_messages:
-        return mq_out.receive()[0][2:-1]
+        return str(mq_out.receive()[0])[2:-1]
     return ''
 
 def send(s):
